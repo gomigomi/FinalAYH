5471a2e370f1810bbd287ac4332d29d894ba1d4a
John <johnnam31@gmail.com>
2015-04-15 19:57:58 +0900

    src 경로   bac -> hungry로 변경

--- src/com/hungry/img/imgDao.java
+++ src/com/hungry/img/imgDao.java
@@ -1,4 +1,4 @@
-package com.bac.img;
+package com.hungry.img;
 
 public class imgDao {
 
--- src/com/hungry/img/imgServlet.java
+++ src/com/hungry/img/imgServlet.java
@@ -1,4 +1,4 @@
-package com.bac.img;
+package com.hungry.img;
 
 import java.io.File;
 import java.io.IOException;
