33417fb8403d5f82ffe1c6623265810e7117c95f
John <johnnam31@gmail.com>
2015-04-25 21:40:50 +0900

    별거없음 해결해야할 것 :  1. getFavoriteView에서 쿼리문이 로우를 하나만 가져오는 현상 			2. getPosting에서 유저를 구분해서 가져오는

--- src/com/hungry/favorite/FavoriteDao.java
+++ src/com/hungry/favorite/FavoriteDao.java
@@ -54,8 +54,8 @@ public class FavoriteDao {
 					"FROM posting AS A "+
 					"LEFT OUTER JOIN user B ON B.id = A.writer "+ 
 					"LEFT OUTER JOIN comment C ON C.posting_seq = A.seq "+ 
-					"JOIN bookmark D ON D.posting_seq = A.seq "+
-					"WHERE D.id = '"+id+"'"+
+					"LEFT OUTER JOIN bookmark D ON D.posting_seq = A.seq "+
+					"WHERE A.writer = '"+id+"'"+
 					"GROUP BY A.seq "+
 					"ORDER BY A.seq DESC";
 
